1ba264dc822cbf19d2c4757e52919513fba311cc
Craz3Squirr3l <mstein999@gmail.com>
2019-11-20 16:05:58 -0500

    Took care of missing data and encoding data

--- Data Preprocessing/data-preprocessing.py
+++ Data Preprocessing/data-preprocessing.py
@@ -1,7 +1,7 @@
 # Data Preprocessing
 
 # importing libraries
-import numpy as numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -13,3 +13,23 @@ X = dataset.iloc[:, :-1].values
 
 # Create dependant variable vector
 y = dataset.iloc[:, 3].values
+
+# taking care of missing data
+from sklearn.impute import SimpleImputer
+missingvalues = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)
+missingvalues = missingvalues.fit(X[:, 1:3])
+X[:, 1:3]=missingvalues.transform(X[:, 1:3])
+
+
+# Encoding categorical data
+#===========================
+# Encoding independent variable (countries)
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X), dtype=np.float)
+
+# Encoding Y data (purchased)
+from sklearn.preprocessing import LabelEncoder
+y = LabelEncoder().fit_transform(y)
+
