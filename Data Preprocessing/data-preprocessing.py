aea9efdf98aec869df2247256feef353ed284101
Craz3Squirr3l <mstein999@gmail.com>
2019-11-21 18:44:02 -0500

    Fixed dataset paths for panda

--- Data Preprocessing/data-preprocessing.py
+++ Data Preprocessing/data-preprocessing.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # import the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv(r'Data Preprocessing\Data.csv')
 
 # create matrix of features
 X = dataset.iloc[:, :-1].values
